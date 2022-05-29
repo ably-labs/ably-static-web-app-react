deb123e3a39858f2f013d405e16508e69a56d8dd
Jo Franchetti <jofranchetti@gmail.com>
2022-05-29 18:52:26 +0100

    updating code to use createRoot for react 17 and above

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 import { configureAbly } from "@ably-labs/react-hooks";
 
 function generateUuid() {
-    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
 const optionalClientId = generateUuid();
 configureAbly({ authUrl: `/api/ably/token-request?clientId=${optionalClientId}`, clientId: optionalClientId });
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
